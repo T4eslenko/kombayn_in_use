3501ea830515bea8978e86706fd9e6a0f254215e
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-04 22:01:43 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -53,20 +53,16 @@ def config():
             if file.endswith('.session'):
                 sessions.append(file)
 
-prompt_message = (
-    "1 - Обновить api_id [{options[0].replace('\n', '')}]\n"
-    "2 - Обновить api_hash [{options[1].replace('\n', '')}]\n"
-    "3 - Парсить user-id [{options[2].replace('\n', '')}]\n"
-    "4 - Парсить user-name [{options[3].replace('\n', '')}]\n"
-    f"5 - Добавить аккаунт юзербота[{len(sessions)}]\n"
-    "6 - Сбросить настройки\n"
-    "e - Выход\n"
-    "Ввод: "
-)
+        key = str(input((f"1 - Обновить api_id [{options[0].replace('\\n', '')}]\n"
+                         f"2 - Обновить api_hash [{options[1].replace('\\n', '')}]\n"
+                         f"3 - Парсить user-id [{options[2].replace('\\n', '')}]\n"
+                         f"4 - Парсить user-name [{options[3].replace('\\n', '')}]\n"
+                         f"5 - Добавить аккаунт юзербота[{len(sessions)}]\n"
+                         "6 - Сбросить настройки\n"
+                         "e - Выход\n"
+                         "Ввод: ")
+                    ))
 
-key = str(input(prompt_message))
-
-        
         if key == '1':
             os.system('cls||clear')
             options[0] = str(input("Введите API_ID: ")) + "\n"
