1df66e0498a9f3638f494702b55b10e4ed4a217c
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-04 22:25:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ if __name__ == "__main__":
                             "e - Выход\n"
                             "Ввод: "))
         
-        # Остальной код
+        if selection == '1':
+            config()
 
         elif selection == '2':
             # Остальной код для парсинга
@@ -57,3 +58,43 @@ if __name__ == "__main__":
 
             # Опционально, можно удалить файл после отправки
             os.remove(file_path)
+
+        elif selection == '3':
+            with open('usernames.txt', 'r') as f:
+                users = list(f)
+
+            print("Выберите юзер-бота для инвайтинга.\n"
+                "(Аккаунт который состоит в группе, в которую производится инвайт)")
+            
+            sessions = []
+            for file in os.listdir('.'):
+                if file.endswith('.session'):
+                    sessions.append(file)
+
+            for i in range(len(sessions)):
+                print(f"{i} -", sessions[i])
+            i = int(input("Ввод: "))
+            
+            client = TelegramClient(sessions[i].replace('\n', ''), api_id, api_hash)
+
+            channelname = input('Введите имя канала для инвайта (без "@")')
+
+            for limit in range(20):
+                try:
+                    inviting(client, channelname, users[limit].replace('\n', ''))
+                    print(users[limit].replace('\n', ''))
+                    time.sleep(random.randrange(15, 40))
+
+                except UserPrivacyRestrictedError:
+                    print('Пользователь ' + users[limit].replace('\n', '') + ' запретил его инвайтить. Пропускаем :(')
+
+                except PeerFloodError:
+                    print('Телеграмм заспамлен.')
+                    break
+
+                except Exception as error:
+                    print(error)
+                    break
+
+        elif selection == 'e':
+            break
