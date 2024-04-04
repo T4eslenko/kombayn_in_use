b5c88416026a99d111dffac6318612e80bf84594
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-04 22:07:32 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -50,16 +50,16 @@ def config():
         options = getoptions()
         sessions = [file for file in os.listdir('.') if file.endswith('.session')]
 
-        prompt_message = (
-            f"1 - Обновить api_id [{options[0].replace('\\\\n', '')}]\n"
-            f"2 - Обновить api_hash [{options[1].replace('\\\\n', '')}]\n"
-            f"3 - Парсить user-id [{options[2].replace('\\\\n', '')}]\n"
-            f"4 - Парсить user-name [{options[3].replace('\\\\n', '')}]\n"
-            f"5 - Добавить аккаунт юзербота[{len(sessions)}]\n"
-            "6 - Сбросить настройки\n"
-            "e - Выход\n"
-            "Ввод: "
-        )
+prompt_message = (
+    f"1 - Обновить api_id [{options[0].replace('\n', '')}]\n"
+    f"2 - Обновить api_hash [{options[1].replace('\n', '')}]\n"
+    f"3 - Парсить user-id [{options[2].replace('\n', '')}]\n"
+    f"4 - Парсить user-name [{options[3].replace('\n', '')}]\n"
+    f"5 - Добавить аккаунт юзербота[{len(sessions)}]\n"
+    "6 - Сбросить настройки\n"
+    "e - Выход\n"
+    "Ввод: "
+)
 
         key = str(input(prompt_message))
 
