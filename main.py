0d8cc0676bd3ce60e51e8b9d519ffe1257bcd4ca
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-06 16:31:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,19 @@ if __name__ == "__main__":
         
         elif selection == '5':
             get_contacts()
+            print("Выберите аккаунт для получения списка контактов.\n"
+
+            sessions = []
+            for file in os.listdir('.'):
+                if file.endswith('.session'):
+                    sessions.append(file)
+
+            for i in range(len(sessions)):
+                print(f"[{i}] -", sessions[i], '\n')
+            i = int(input("Ввод: "))
+            
+            client = TelegramClient(sessions[i].replace('\n', ''), api_id, api_hash).start(sessions[i].replace('\n', ''))
+
             
         elif selection == '2':
             chats = []
