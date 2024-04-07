d854c7fa2040a7c7c1f92df46a4f5fa7352c5b30
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-07 12:13:41 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -102,7 +102,7 @@ def inviting(client, channel, users):
     ))
 
 # Новая функция
-def parsing_xlsx(client, index: int, id: bool, name: bool, g_title):
+def parsing_xlsx(client, index: int, id: bool, name: bool):
     all_participants = client.get_participants(index)
 
     # Создание нового документа Excel
