c2e193186698eabb36a8a822031b5958eafa85f8
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-21 22:24:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
                             for chat in chats:
                                 try:
                                     for chat in chats:
-                                        if chat.channel_id is None and chat.megagroup is None:
+                                        if chat.megagroup is True:
                                             groups.append(chat)
                                 except Exception as e:
                                     print(f"Error: {e}")
