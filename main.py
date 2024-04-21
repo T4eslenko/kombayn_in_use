284f4c0dcdbf79f454bc56531303679348d26fd5
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-21 21:22:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -193,8 +193,9 @@ if __name__ == "__main__":
                             chats.extend(result.chats)
                             for chat in chats:
                                 try:
-                                    if chat.megagroup or (not hasattr(chat, 'megagroup') and not hasattr(chat, 'channel')):
+                                    if getattr(chat, 'megagroup', False) or not getattr(chat, 'channel', False):
                                         groups.append(chat)
+                                        print(chat.stringify())
                                 except Exception as e:
                                     print(f"Error: {e}")
 
