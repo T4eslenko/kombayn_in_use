2b595a7dad2524c08044cb6155665c3b98640f2c
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-21 23:12:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
                                 try:
                                     #if chat.megagroup is True:
                                     #if not str(chat.id).startswith('-') and not isinstance(chat, Channel):
-                                    if not isinstance(chat, Channel)
+                                    if not isinstance(chat, Channel):
                                     # and not getattr(chat, 'megagroup', False):
                                     #if not isinstance(chat, Channel) and not getattr(chat, 'megagroup', False) and not getattr(chat, 'gigagroup', False):
                                         groups.append(chat)
