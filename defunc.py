bd7c3e991784fbf7ece6500a14fea94958aac859
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-22 22:45:11 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -167,8 +167,6 @@ def inviting(client, channel, users):
 # Выгружаем сообщения чата
 def remove_timezone(dt):
     # Удаление информации о часовом поясе из объекта datetime
-    if dt is None:
-        return None
     if dt.tzinfo:
         dt = dt.astimezone().replace(tzinfo=None)
     return dt
@@ -176,8 +174,6 @@ def remove_timezone(dt):
 def get_message_info(client, group_title, msg_id):
     # Получение информации о сообщении
     message = client.get_messages(group_title, ids=[msg_id])[0]
-    if message is None:
-        return None, None, None, None, None, None
     user_id = message.sender_id if isinstance(message.sender, User) else None
     username = message.sender.username if isinstance(message.sender, User) else None
     first_name = message.sender.first_name if isinstance(message.sender, User) else None
@@ -192,8 +188,6 @@ def parsing_messages(client, index: int, id: bool, name: bool, group_title):
     for message in client.iter_messages(group_title, limit=None):
         # Основная информация о сообщении
         user_id, username, first_name, last_name, date, text = get_message_info(client, group_title, message.id)
-        if date is None:
-            continue
         row_data = [
             message.chat_id,
             message.id,
@@ -209,8 +203,6 @@ def parsing_messages(client, index: int, id: bool, name: bool, group_title):
         if isinstance(message.reply_to_msg_id, int):
             reply_msg_id = message.reply_to_msg_id
             reply_user_id, reply_username, reply_first_name, reply_last_name, reply_date, reply_text = get_message_info(client, group_title, reply_msg_id)
-            if reply_date is None:
-                continue
             row_data.extend([
                 reply_text,
                 reply_user_id,
@@ -229,8 +221,6 @@ def parsing_messages(client, index: int, id: bool, name: bool, group_title):
     # Сохраняем книгу Excel с названием, содержащим group_title
     filename = f"{group_title}_messages.xlsx"
     wb.save(filename)
-
-
 
 
 
