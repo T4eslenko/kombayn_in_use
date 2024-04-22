577588a8fa46cfd69b04dd2d87d0e6513d060523
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-22 07:36:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ if __name__ == "__main__":
                             for chat in chats:
                                 try:
                                     #if chat.megagroup:
+                                    if chat.Chat:
                                         groups.append(chat)
                                 except:
                                     continue
@@ -207,8 +208,8 @@ if __name__ == "__main__":
                                 i = 0
                                 print('-----------------------------')
                                 for g in groups:
-                                    #print(str(i) + ' - ' + g.title)
-                                    print(g)
+                                    print(str(i) + ' - ' + g.title)
+                                    #print(g)
                                     i+=1
                                 g_index_str = str(input("Ввод: "))
                        
