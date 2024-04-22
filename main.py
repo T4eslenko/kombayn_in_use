904655868c56ec005fa3f07a666b78b61dd635df
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-22 08:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
                             for chat in chats:
                                 try:
                                     #if chat.megagroup:
-                                    if chat.broadcast is False:
+                                    if chat.broadcast is None:
                                         groups.append(chat)
                                 except:
                                     continue
