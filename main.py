a13ab3f089a052933e204100660963de020843c8
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-25 14:55:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -124,9 +124,6 @@ if __name__ == "__main__":
                                    if isinstance(chat, Chat) and chat.migrated_to is None:
                                       closechats.append(chat)
                                       groups.append(chat)
-                                   if chat.megagroup and chat.username==None:
-                                      closechats.append(chat)
-                                      groups.append(chat)
 
                                    
                                    if chat.broadcast and chat.username:
