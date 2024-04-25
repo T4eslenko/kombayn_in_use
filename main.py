fcd939cda8b15bb3066aec8e7b1b066a6cc10368
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-25 14:52:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
                            #Парсим информацию обо всех группах
                             for chat in chats:
                                 #try:
-                                   if chat.broadcast:
+                                   if isinstance(chat, Chat) and if chat.broadcast:
                                      if chat.broadcast==False and chat.username==None:
                                         closechats.append(chat)
                                         groups.append(chat)
