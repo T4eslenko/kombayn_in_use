8657703ed5cc1230b8f7b3cb737a523e6146636b
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-28 19:35:30 +0300

    Update groups.py

--- groups.py
+++ groups.py
@@ -39,7 +39,7 @@ def channelandgroups(api_id, api_hash):
                     me = client.get_me()
                     userid = me.id
                     first_name = me.first_name
-                    username = f"{me.username}" if me.username is not None else ""
+                    username = f"@{me.username}" if me.username is not None else ""
                     lastname = me.last_name if me.last_name is not None else ""
 
                                            
@@ -90,7 +90,7 @@ def channelandgroups(api_id, api_hash):
                         owner_closechannel = 0
                         print('-----------------------------')
                         print("=ИНФОРМАЦИЯ О КАНАЛАХ И ЧАТАХ=")
-                        print(f"{userid}, {username}, {first_name}, {lastname}")
+                        print(f"ID: {userid}, {username}, ({first_name} {lastname})")
                         print('-----------------------------')
                         print()
                         print("\033[95mОткрытые КАНАЛЫ:\033[0m")
