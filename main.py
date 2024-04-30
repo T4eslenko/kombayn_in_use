3c7000103e4d3e1bacbc2c992c32e927514f7884
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-04-30 11:11:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
                                 print(f"\033[96mНомер телефона: +{phone}, ID: {userid}, ({firstname}{lastname}) {username}\033[0m")
                                 print('-----------------------------')
                                 for g in groups:
-                                    username = f"@{g.username}" if g.username is not None else ""
+                                    username = f"@{g.username}" if hasattr(g, 'username') and g.username is not None else ""
                                     if g.creator:
                                        print(str(i) + ' - ' + g.title + '\033[93m [' + str(g.participants_count) + ']\033[0m' + color.RED + ' (Владелец)' + color.END + color.BLUE + ' ' + username + color.END)
                                     elif g.admin_rights is not None:
