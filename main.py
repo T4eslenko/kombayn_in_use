2635e961f713664d17b275160716bce875abc234
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-01 21:45:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -504,22 +504,18 @@ if __name__ == "__main__":
       
                           chats = client.get_dialogs()
                           for chat in chats:
-                              if isinstance(chat.entity, Channel) or isinstance(chat.entity, Chat): #проверяем групповой ли чат
-                                  
+                              if isinstance(chat.entity, Channel) or isinstance(chat.entity, Chat): #проверяем групповой ли чат  
                                   # Определяем открытый чат
                                   if isinstance(chat.entity, Channel) and hasattr(chat.entity, 'broadcast'):
                                       if chat.entity.broadcast == False and chat.entity.username:
-                                          groups.append(chat.entity)
-                                  
+                                          openchats.append(chat.entity)
                                   # Определяем закрытый чат
                                   if isinstance(chat.entity, Channel) and hasattr(chat.entity, 'broadcast'):
                                       if chat.entity.broadcast == False and chat.entity.username == None:
-                                          groups.append(chat.entity)
+                                          closechats.append(chat.entity)
                                   if isinstance(chat.entity, Chat) and chat.entity.migrated_to is None:
-                                      groups.append(chat.entity)    
-      
-      
-                              
+                                      closechats.append(chat.entity)
+                         
                           while True:
                               os.system('cls||clear')
                               i = 0
