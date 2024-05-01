38544b947027acebca56048df7db37cc4909cf1e
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-01 13:51:37 +0300

    Update groupsinfo.py

--- groupsinfo.py
+++ groupsinfo.py
@@ -100,7 +100,7 @@ def channelandgroups(api_id, api_hash, print_pages, print_channel_lists):
 
                         
                         # Код для формирования списков openchannels, closechannels, openchats и closechats...
-                        print_channel_lists(openchannels, closechannels, openchats, closechats)
+                        print_channel_lists(openchannels, closechannels, openchats, closechats, print_pages)
 
 
                         print()
