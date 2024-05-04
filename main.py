4f3e11dbe85745cf539290ba761c0f76290b643f
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-04 10:01:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,6 +112,7 @@ if __name__ == "__main__":
                             client.connect()
 
                             # Получение информации о пользователе
+                            get_user_info(client, session)
                             userid, user_info = get_user_info(client, session)
                            
                             #me = client.get_me()
