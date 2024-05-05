07200e6c21733001631f17fa065cb240f4499601
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-05 16:48:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,12 +69,12 @@ def get_type_of_chats(client, selection):
     for chat in chats:
 
         if isinstance(chat.entity, Channel) or isinstance(chat.entity, Chat): # проверяем групповой ли чат
-             if isinstance(chat.entity, Chat):
-                if chat.entity.migrated_to:
-                   all_chats_ids.append(chat.entity.migrated_to.channel_id)
-             elif isinstance(chat.entity, Channel):
-                if chat.entity.migrated_to:
-                   all_chats_ids.append(chat.entity.migrated_to.channel_id)
+            if isinstance(chat.entity, Chat):
+               if chat.entity.migrated_to:
+                  all_chats_ids.append(chat.entity.migrated_to.channel_id)
+            elif isinstance(chat.entity, Channel):
+               if chat.entity.migrated_to:
+                  all_chats_ids.append(chat.entity.migrated_to.channel_id)
 
            
             if selection == '7': #выгружаем количество сообщений при функции выгрузить сообщение
