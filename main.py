0648072a4795b50dc569b63c5237ed25eebdc5d1
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-05 15:48:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,8 @@ def get_type_of_chats(client, selection):
     for chat in chats:
 
         if isinstance(chat.entity, Channel) or isinstance(chat.entity, Chat): # проверяем групповой ли чат
+            if isinstance(chat.entity, Chat):
+               all_chats_ids.append(chat.entity.id)
            
             if selection == '7': #выгружаем количество сообщений при функции выгрузить сообщение
                 messages = client.get_messages(chat.entity, limit=0)
@@ -99,9 +101,6 @@ def get_type_of_chats(client, selection):
             if selection == '5': #Добавляем нулевые чаты для общей информации
                if isinstance(chat.entity, Chat) and hasattr(chat.entity, 'participants_count') and chat.entity.participants_count == 0:
                   if hasattr(chat.entity, 'migrated_to'):
-                     
-                     if isinstance(chat.entity, Chat):
-                        all_chats_ids.append(chat.entity.id)
            
                      if isinstance(chat.entity, Chat) and hasattr(chat.entity, 'migrated_to') and getattr(chat.entity.migrated_to, 'channel_id', None) is not None:
                         all_chat_deleted.append(getattr(chat.entity.migrated_to, 'channel_id', None))     
