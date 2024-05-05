b7854010c375ac6559123d27a9d958712aff3852
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-05 17:43:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,9 @@ def get_type_of_chats(client, selection):
         if isinstance(chat.entity, Channel) or isinstance(chat.entity, Chat): # проверяем групповой ли чат
             if hasattr(chat, 'deactivated') and chat.deactivated == True and hasattr(chat, 'migrated_to') and isinstance(chat.migrated_to, InputChannel):
                deactivated_chats.append(chat.migrated_to.channel_id)
+               print(deactivated_chats)
+               print(all_chats_ids)
+               input("жмяк 1")     
             
 
            
