86fe9436dc7564dc2b5b172462ca65b8ec1b1317
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-05 14:08:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def get_type_of_chats(client, selection):
                if isinstance(chat.entity, Chat) and hasattr(chat.entity, 'participants_count') and chat.entity.participants_count == 0:
                   if hasattr(chat.entity, 'migrated_to'):
                       migrated_channel_id = chat.entity.migrated_to.channel_id
-                      if any(chat.entity.migrated_to.channel_id != migrated_channel_id for entity in chats):
+                      if any(chat.entity.migrated_to.channel_id != migrated_channel_id for chat.entity.migrated_to.channel_id in chats):
                          closechats.append(chat.entity)
 
                                
