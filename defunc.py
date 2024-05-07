ad8949d62974ea3ecc6c99db51abb0945cbb7303
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-07 13:06:01 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -19,7 +19,7 @@ from datetime import datetime
 from typing import Optional
 import re
 
-def type_of_channel(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats):
+def make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats):
              owner_channel = 0
              owner_group = 0
              owner_closegroup = 0
@@ -83,6 +83,8 @@ def type_of_channel(delgroups, chat_message_counts, openchannels, closechannels,
             		 owner_group += 1
             		 owner_closegroup += 1                    
              return all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup
+
+
 #Запись информации о группах в файл
 def write_data(sheet, data):
     sheet.append(["Название", "Количество участников", "Владелец", "Администратор", "ID", "Ссылка"])
