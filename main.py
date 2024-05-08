cf3277944d64ec070f5928478b4f4b38cc01aa19
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-08 16:40:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
            print('-----------------------------')
            print()
            client, phone, session_index = choice_akk(api_id, api_hash) if choice_akk(api_id, api_hash) is not None else (None, None, None)
-           if 'user_input' in globals() and user_input.lower() == 'e':
+           if session_index is None:
               break
            else: 
               print(e)
