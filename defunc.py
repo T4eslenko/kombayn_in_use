7df4d300c2706508cff1bdcf0e18a0754b6bfac6
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-08 22:19:16 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -162,6 +162,8 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
 
     
     delgroups_name = 'Удаленные ГРУППЫ:' if delgroups is not None else ''
+    print(delgroups)
+    input()
     all_info.append(f"\033[95m{delgroups_name}\033[0m")
     closegroupdel_count = 1
     for delgroup in delgroups:
