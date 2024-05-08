efa081d604457bfdbe939f0db86b9bf563a70b19
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-08 16:39:21 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -32,7 +32,7 @@ def choice_akk(api_id, api_hash):
         user_input = input("\033[92mДля продолжения выберите существующий аккаунт ('e' - назад): \033[0m")
         if user_input.lower() == 'e':
             user_input = 'ec'
-            return
+            return user_input
         else:
             try:
                 session_index = int(user_input)
