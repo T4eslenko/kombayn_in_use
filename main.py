20a0f588bb64faf22318ab5829aeb3adbfaa5ea2
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-08 17:18:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,6 @@ if __name__ == "__main__":
            if result is None:
                continue
            client, phone, session_index = result
-           #client, phone, session_index = choice_akk(api_id, api_hash) if choice_akk(api_id, api_hash) is not None else (None, None, None)
-           #if session_index is None:continue
            userid, userinfo, firstname, lastname, username = get_user_info(client, phone) # Получение информации о пользователe
            print('=ИНФОРМАЦИЯ О КОНТАКТАХ=')
            print('-----------------------------')
