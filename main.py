2e8d5fb61d22eac3b82052780482030d227ff290
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-08 22:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -111,6 +111,10 @@ if __name__ == "__main__":
            client, phone, session_index = result
            userid, userinfo, firstname, lastname, username = get_user_info(client, phone) # Получение информации о пользователe
            print()
+           groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup = make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats)
+           print_suminfo_abou_channel(openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup)
+           input("\033[93mНажмите любую клавишу для продолжения... \033[0m")
+           os.system('cls||clear')
            print('-----------------------------')
            print('=ИНФОРМАЦИЯ О КАНАЛАХ и ГРУППАХ=')
            print('-----------------------------')
@@ -119,8 +123,6 @@ if __name__ == "__main__":
            while True:
                print(f"\033[96mНомер телефона: +{phone}, ID: {userid}, ({firstname}{lastname}) {username}\033[0m")
                print('-----------------------------')
-               groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup = make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats)
-               print_suminfo_abou_channel(openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup)
                print()
                input("\033[93mНажмите любую клавишу для продолжения... \033[0m")
                print()
@@ -156,8 +158,6 @@ if __name__ == "__main__":
               client, phone, session_index = result
               userid, userinfo, firstname, lastname, username = get_user_info(client, phone) # Получение информации о пользователe
               print()
-              groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup = make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats)
-              print_suminfo_abou_channel (openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_channel, owner_closechannel, owner_group, owner_closegroup)
               input("\033[93mНажмите любую клавишу для продолжения... \033[0m")
               print()
               delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats = get_type_of_chats(client, selection)  # Получение информации о чатах и каналах
