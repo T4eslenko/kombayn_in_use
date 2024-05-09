3bfc916e61b00ac36b9822eec5dba016c6048ffc
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-09 12:48:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ if __name__ == "__main__":
                               print('Может потребоваться значительное количество времени, заварите кофе...')
                               get_messages_and_save_xcls(client, target_group, user_id, user_name, group_title, userid, userinfo)
                               group_id = target_group.id
-                              if group_id not in openchannels or closechannels:
+                              if group_id not in openchannels.id or closechannels.id:
                                  get_participants_and_save_xlsx(client, target_group, user_id, user_name, group_title, group_id, userid, userinfo)
                               os.system('cls||clear')
                               print('Сообщения чата выгружены в excel, мой командир')
