cfcf0c83cedfd27cd67c8078a7a9799abde1e790
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-11 10:35:43 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -492,14 +492,15 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
             last_name,
             text
         ]
-        if user_id not in participants_from_messages[0]:
-            if user_id:
-                participants_from_messages.append([
-                    user_id,
-                    f"@{username}" if username else None,
-                    first_name,
-                    last_name
-                ])
+        if participants_from_messages:
+            if user_id not in participants_from_messages[0]:
+                if user_id:
+                    participants_from_messages.append([
+                        user_id,
+                        f"@{username}" if username else None,
+                        first_name,
+                        last_name
+                    ])
 
         # Если сообщение является ответом на другое сообщение
         if isinstance(message.reply_to_msg_id, int):
@@ -516,14 +517,15 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
                 reply_msg_id,
                 remove_timezone(reply_date)
             ])
-            if reply_user_id not in participants_from_messages[0]:
-                if reply_user_id:
-                    participants_from_messages.append([
-                        reply_user_id,
-                        f"@{reply_username}" if reply_username else None,
-                        reply_first_name,
-                        reply_last_name
-                    ])
+            if participants_from_messages:
+                if reply_user_id not in participants_from_messages[0]:
+                    if reply_user_id:
+                        participants_from_messages.append([
+                            reply_user_id,
+                            f"@{reply_username}" if reply_username else None,
+                            reply_first_name,
+                            reply_last_name
+                        ])
 
         else:
             row_data.extend([None] * 7)
