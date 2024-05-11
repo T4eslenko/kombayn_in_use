ee291e7d8bdb9bfae35e9339ad878f256c7118c6
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-11 21:35:54 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -33,6 +33,8 @@ def get_message_info(message):
     username = message.sender.username if isinstance(message.sender, User) else None
     first_name = message.sender.first_name if isinstance(message.sender, User) else None
     last_name = message.sender.last_name if isinstance(message.sender, User) else None
+    fwd_user_id = message.fwd_from.from_id.user_id if isinstance(message.fwd_from, MessageFwdHeader) else None
+    fwd_date = message.fwd_from.date if isinstance(message.fwd_from, MessageFwdHeader) else None
     return user_id, username, first_name, last_name, message.date, message.text
 
 def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_title, userid, userinfo):
