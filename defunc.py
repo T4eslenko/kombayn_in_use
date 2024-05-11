8c07ae134daa02ab6eb72b647c566fd056d06b24
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-11 10:31:42 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -492,7 +492,7 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
             last_name,
             text
         ]
-        if user_id not in participants_from_messages[0]:
+        if participants_from_messages and user_id not in participants_from_messages[0]:
             if user_id:
                 participants_from_messages.append([
                     user_id,
@@ -516,7 +516,7 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
                 reply_msg_id,
                 remove_timezone(reply_date)
             ])
-            if reply_user_id not in participants_from_messages[0]:
+            if participants_from_messages and reply_user_id not in participants_from_messages[0]:
                 if reply_user_id:
                     participants_from_messages.append([
                         reply_user_id,
