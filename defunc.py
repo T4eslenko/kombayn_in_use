9c83d107795acc8eaf4aad5e28287e9cb9343c6f
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-12 11:29:32 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -49,9 +49,9 @@ def get_message_info(message):
     fwd_date = message.fwd_from.date if isinstance(message.fwd_from, MessageFwdHeader) and hasattr(message.fwd_from, 'date') else None
     if fwd_user_id or fwd_channel_id:
         if fwd_user_id:
-            fwd_source_id = fwd_user_id
+            fwd_source_id = f"(From user: {fwd_user_id})"
         else:
-            fwd_source_id = fwd_channel_id
+            fwd_source_id = f"(From channel: {fwd_channel_id})"
 
     return user_id, username, first_name, last_name, date, text, media, fwd_source_id, fwd_date
 
