95968a50677a8d9d7489e6b82713d90f2863b19c
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-13 12:26:40 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -32,7 +32,12 @@ def get_message_info(message):
     if message is None:
         return None, None, None, None, None, None, None, None, None
     #sender_id = message.sender_id if hasattr(message, 'sender_id') else None
-    sender_id = message.sender_id if hasattr(message, 'sender_id') else getattr(message.to_id, 'channel_id', None)
+    if hasattr(message, 'sender_id'):
+        sender_id = message.sender_id
+    elif hasattr(message, 'to_id') and hasattr(message.to_id, 'channel_id'):
+        sender_id = message.to_id.channel_id
+    else:
+        sender_id = 'Admin'
     username = message.sender.username if hasattr(message.sender, 'username') else None
     first_name = message.sender.first_name if hasattr(message.sender, 'first_name') else None
     last_name = message.sender.last_name if hasattr(message.sender, 'last_name') else None
