02c31e152cda1aacaa6472c95f413b663cb73467
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-13 21:35:22 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -83,16 +83,17 @@ def get_message_info(message):
     reaction_info = ""
     reactions = message.reactions
     if reactions:
-        for reaction in message.reactions.recent_reactions:
-            print(reaction)
-            input("reaction")
-            user_id = reaction.peer_id.user_id
-            reaction_emoji = reaction.reaction.emoticon
-            reaction_info += f"Пользователь с ID {user_id} оставил реакцию {reaction_emoji}\n"
-    
-        print(reaction_info)
-    else:
-        print("Реакций на сообщение нет.")
+        if hasattr(reactions, 'recent_reactions'):
+            for reaction in message.reactions.recent_reactions:
+                print(reaction)
+                input("reaction")
+                user_id = reaction.peer_id.user_id
+                reaction_emoji = reaction.reaction.emoticon
+                reaction_info += f"Пользователь с ID {user_id} оставил реакцию {reaction_emoji}\n"
+        
+            print(reaction_info)
+        else:
+            print("Реакций на сообщение нет.")
     return sender_id, username, first_name, last_name, date, text, media_type, fwd_source_id, fwd_date, reaction_info
 
 def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_title, userid, userinfo):
