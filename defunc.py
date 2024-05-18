6a6e172622016285d4e2deba35242832700b163d
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-18 17:44:25 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -506,8 +506,9 @@ def get_and_save_contacts(client, phone, userinfo, userid):
     print(f"Количество взаимных контактов: {total_mutual_contacts}")
     print()
 
+# Заблокированные пользователи
 def get_blocked_bot(client):
-    blocked_bot_info = []
+    blocked_bot_info = "заблокированные БОТЫ:"
     count_blocked_bot = 0
     earliest_date = None
     latest_date = None
@@ -520,7 +521,7 @@ def get_blocked_bot(client):
                 bot_name = user.first_name
                 bot_at = user.username
                 date_blocked = peer.date.strftime("%d/%m/%Y")
-                blocked_bot_info.append(f"заблокированные БОТЫ: ID:{user.id}, @{user.username}, {user.first_name}, заблокирован:{peer.date.strftime('%d/%m/%Y')}")
+                blocked_bot_info.append(f"ID:{user.id}, 033[36m@{user.username}\033[0m, \033[93m"{user.first_name}"\033[0m, заблокирован:{peer.date.strftime('%d/%m/%Y')}")
                 if earliest_date is None or peer.date < earliest_date:
                     earliest_date = peer.date
                 if latest_date is None or peer.date > latest_date:
