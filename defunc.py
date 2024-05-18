b4277cd7d86c02f94869c36f57429c47af3dea40
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-18 23:56:21 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -698,7 +698,7 @@ def get_participants_and_save_xlsx(client, index: int, id: bool, name: bool, gro
     
 # Функци по отправке в боты
 def send_files_to_bot(bot, admin_chat_ids):
-    file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx']
+    file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx', '_repot.html']
 
     for file_extension in file_extensions:
         files_to_send = [file_name for file_name in os.listdir('.') if file_name.endswith(file_extension) and os.path.getsize(file_name) > 0]
