30c8a4e39f10110437c98688e25bb65f1e1a88ac
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-26 12:55:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,114 @@
+
+#Настройки
+def config(api_id, api_hash, selection, bot, admin_chat_ids):
+    while True:
+        os.system('cls||clear')
+
+        with open('options.txt', 'r+') as f:
+            if not f.readlines():
+                f.write("NONEID\n"
+                        "NONEHASH\n"
+                        "True\n"
+                        "True\n")
+                continue
+                
+        options = getoptions()
+        sessions = getsessions()
+
+        prompt_message = (
+            f"\033[35m1 - Обновить api_id \033[0m\033[37m[{options[0].rstrip()}]\033[0m\n"
+            f"\033[35m2 - Обновить api_hash \033[0m\033[37m[{options[1].rstrip()}]\033[0m\n"
+            "\033[31m3 - Сбросить настройки\033[0m\n"
+            " \n"
+            f"\033[32m4 - Парсить user-id\033[0m \033[37m[{options[2].rstrip()}]\033[0m\n"
+            f"\033[32m5 - Парсить user-name\033[0m \033[37m[{options[3].rstrip()}]\033[0m\n"
+            " \n"
+            "\033[96m\033[4mРабота с аккаунтами\033[0m\033[0m\n"
+            f"\033[36m6 - Вывести список подключенных аккаунтов.\033[0m \033[37mСейчас: [{len(sessions)}]\033[0m\n"
+            f"\033[36m7 - Добавить новый аккаунт.\033[0m \033[37mСейчас: [{len(sessions)}]\033[0m\n"
+            f"\033[36m8 - Завершить сеанс аккаунта в системе.\033[0m \033[37mСейчас: [{len(sessions)}]\033[0m\n"
+            " \n"
+            "\033[33m'e' - Назад\033[0m\n"
+            " \n"
+            "\033[37mВвод: \033[0m"
+        )
+
+        key = str(input(prompt_message))
+
+        if key == '1':
+            os.system('cls||clear')
+            options[0] = str(input("Введите API_ID: ")) + "\n"
+
+        elif key == '2':
+            os.system('cls||clear')
+            options[1] = str(input("Введите API_HASH: ")) + "\n"
+
+        elif key == '4':
+            if options[2] == 'True\n':
+                options[2] = 'False\n'
+            else:
+                options[2] = 'True\n'
+
+        elif key == '5':
+            if options[3] == 'True\n':
+                options[3] = 'False\n'
+            else:
+                options[3] = 'True\n'
+
+# Просмотреть подключенные аккаунты
+        elif key == '6':
+            os.system('cls||clear')
+            if options[0] == "NONEID\n" or options[1] == "NONEHASH":
+                print("Проверьте api_id и api_hash")
+                time.sleep(2)
+                continue
+
+            print("Подключенные аккаунты:\n")
+            for i in sessions:
+                print(i)
+            print()
+            input("\033[93mНажмите Enter для продолжения...\033[0m")
+
+#Добавить аккаунт
+        elif key == '7':
+            add_account(api_id, api_hash, selection, bot, admin_chat_ids)
+            continue
+
+ #Удалить аккаунт     
+        elif key == '8':
+            remouve_account(api_id, api_hash, selection, bot, admin_chat_ids)
+            continue
+
+
+# Сброс настроеек
+        elif key == '3':
+            os.system('cls||clear')
+            answer = input("Вы уверены?\nAPI_ID и API_HASH будут удалены\n"
+                           "1 - Удалить\n2 - Назад\n"
+                           "Ввод: ")
+            if answer == '1':    
+                options.clear()
+                print("Настройки очищены.")
+                time.sleep(2)
+            else:
+                continue
+
+        elif key == 'e':
+            os.system('cls||clear')
+            break
+
+        with open('options.txt', 'w') as f:
+            f.writelines(options)
+
+
+def getoptions():
+    with open('options.txt', 'r') as f:
+        options = f.readlines()
+    return options
+
+def getsessions():
+    sessions = []
+    for file in os.listdir('.'):
+        if file.endswith('.session'):
+            sessions.append(file)
+    return sessions
