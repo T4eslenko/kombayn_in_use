733be6c1042b7a24c2c61257084384157aa41662
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-08 11:36:30 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -67,10 +67,7 @@ def get_user_info(client, phone, selection):
     except Exception as e:
         print(f"An error occurred: {e}")
     
-    result = client(functions.bots.GetBotInfoRequest(
-        lang_code='en',
-        bot='BotFather'
-    ))
+    result = client(functions.account.GetConnectedBotsRequest())
     print(result.stringify())     
 
     input('жми') 
