1dce559928694d6b63f58d80f75f295840c53efa
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-14 17:52:04 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -686,7 +686,7 @@ def get_message_info(message):
     return sender_id, username, first_name, last_name, date, text, media_type, fwd_source_id, fwd_date, reaction_info
 
 def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_title, userid, userinfo, selection):
-  with client.takeout() as takeout: #Добавил
+  #with client.takeout() as takeout: #Добавил
     wb = Workbook()
     ws = wb.active
     ws.cell(row=1, column=1, value=userinfo)
@@ -694,7 +694,10 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
     ws.append(['ID объекта', 'Group ID', 'Message ID', 'Date and Time', 'User ID', '@Username', 'First Name', 'Last Name', 'Message', 'Media', 'Reply to Message', 'Reply to User ID', '@Reply Username', 'Reply First Name', 'Reply Last Name', 'Reply Message ID', 'Reply Date and Time', 'fwd_source_id', 'fwd_date', 'Reactions'])
 
     participants_from_messages = set()
-    all_messages = client.iter_messages(group_title) if selection == '7' else takeout.iter_messages(group_title)
+    all_messages = client.iter_messages(group_title) if selection == '7' else client.get_messages(group_title, limit=None)
+      
+    #all_messages = client.iter_messages(group_title) if selection == '7' else takeout.iter_messages(group_title)  было
+      
     #for message in client.iter_messages(group_title):
     #for message in takeout.iter_messages(group_title):
     for message in all_messages:
@@ -728,7 +731,8 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
             if selection == '7':
                 reply_sender_id, reply_username, reply_first_name, reply_last_name, reply_date, reply_text, reply_media_type, fwd_source_id, fwd_date, reply_reactions = get_message_info(client.get_messages(group_title, ids=[reply_msg_id])[0])  
             if selection == '75':
-                reply_messages = takeout.get_messages(group_title, ids=[reply_msg_id]) #Добавил
+                #reply_messages = takeout.get_messages(group_title, ids=[reply_msg_id]) #Добавил
+                reply_messages = client.get_messages(group_title, ids=[reply_msg_id])
                 if reply_messages: #Добавил
                     reply_message = reply_messages[0] #Добавил
                     reply_sender_id, reply_username, reply_first_name, reply_last_name, reply_date, reply_text, reply_media_type, reply_fwd_source_id, reply_fwd_date, reply_reaction_info = get_message_info(reply_message) #Добавил
