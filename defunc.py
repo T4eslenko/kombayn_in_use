4e890f72c82c50246da0fbd9d30df186eebdf9d3
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-15 17:34:50 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -355,6 +355,47 @@ def get_type_of_chats(client, selection):
 
     return delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, admin_id, user_bots, user_bots_html
 
+# Функция для получения списка прав администратора в канале
+def get_admin_rights_channel_list(admin_rights):
+    rights = ['<span style="color:maroon; font-weight:bold; font-style:italic;">Права, как администратора канала:</span>']
+    possible_rights = {
+        'Изменение профиля канала': admin_rights.change_info if admin_rights else False,
+        'Публикация сообщений': admin_rights.post_messages if admin_rights else False,
+        'Изменение публикаций': admin_rights.edit_messages if admin_rights else False,
+        'Удаление публикаций': admin_rights.delete_messages if admin_rights else False,
+        'Публикация историй': admin_rights.post_stories if admin_rights else False,
+        'Изменение историй': admin_rights.edit_stories if admin_rights else False,
+        'Удаление историй': admin_rights.delete_stories if admin_rights else False,
+        'Пригласительные ссылки': admin_rights.invite_users if admin_rights else False,
+        'Управление трансляциями': admin_rights.manage_call if admin_rights else False,
+        '<b>Назначение администраторов</b>': admin_rights.add_admins if admin_rights else False,
+    }
+    has_any_rights = any(possible_rights.values())
+    for right, has_right in possible_rights.items():
+        status = '<b><span style="color:red; font-weight:bold;">да</span></b>' if has_right else '<b>нет</b>'
+        rights.append(f"{right} - {status}")
+    return rights if has_any_rights else []
+
+def get_admin_rights_chat_list(admin_rights):
+    rights = ['<span style="color:maroon; font-weight:bold; font-style:italic;">Права, как администратора группы:</span>']
+    possible_rights = {
+        'Удаление сообщений': admin_rights.delete_messages if admin_rights else False,
+        'Блокировка пользователей': admin_rights.ban_users if admin_rights else False,
+        'Пригласительные ссылки': admin_rights.invite_users if admin_rights else False,
+        'Закрепление сообщений': admin_rights.pin_messages if admin_rights else False,
+        'Публикация историй': admin_rights.post_stories if admin_rights else False,
+        'Изменение историй': admin_rights.edit_stories if admin_rights else False,
+        'Удаление историй': admin_rights.delete_stories if admin_rights else False,
+        'Управление трансляциями': admin_rights.manage_call if admin_rights else False,
+        '<b>Назначение администраторов</b>': admin_rights.add_admins if admin_rights else False,
+        'Анонимность': admin_rights.anonymous if admin_rights else False
+    }
+    has_any_rights = any(possible_rights.values())
+    for right, has_right in possible_rights.items():
+        status = '<b><span style="color:red; font-weight:bold;">да</span></b>' if has_right else '<b>нет</b>'
+        rights.append(f"{right} - {status}")
+    return rights if has_any_rights else []
+
 
 # Формируем списки каналов и чатов
 def make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, selection, client):
@@ -390,13 +431,20 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
         count_row = openchannel_count if selection == '5' or selection == '0' else i
         owner = " (Владелец)" if openchannel.creator else ""
         admin = " (Администратор)" if openchannel.admin_rights is not None else ""
+        # Получение списка прав администратора
+        admin_rights_list = get_admin_rights_channel_list(openchannel.admin_rights)
+        admin_rights_html = ""
+        if admin_rights_list:
+            admin_rights_html = "<ul style='font-size:14px; font-style:italic;'>" + "".join([f"<li style='margin-left:50px;'>{right}</li>" for right in admin_rights_list]) + "</ul>"
+        
         messages_count = f" / [{chat_message_counts.get(openchannel.id, 0)}]" if chat_message_counts else ""
         all_info.append(f"{count_row} - {openchannel.title} \033[93m[{openchannel.participants_count}]{messages_count}\033[0m\033[91m {owner} {admin}\033[0m ID:{openchannel.id} \033[94m@{openchannel.username}\033[0m")
         public_channels_html.append(
-        f"{openchannel_count}. <img src='{image_data_url}' alt=' ' style='width:50px;height:50px;vertical-align:middle;margin-right:10px;'>" 
-        f"<span style='color:#556B2F;'>{openchannel.title}</span> <span style='color:#8B4513;'>[{openchannel.participants_count}]</span> "
-        f"<span style='color:#FF0000;'>{owner} {admin}</span> ID:{openchannel.id} "
-        f'<a href="https://t.me/{openchannel.username}" style="color:#0000FF; text-decoration: none;">@{openchannel.username}</a>'
+            f"{openchannel_count}. <img src='{image_data_url}' alt=' ' style='width:50px;height:50px;vertical-align:middle;margin-right:10px;'>" 
+            f"<span style='color:#556B2F;'>{openchannel.title}</span> <span style='color:#8B4513;'>[{openchannel.participants_count}]</span> "
+            f"<span style='color:#FF0000;'>{owner} {admin}</span> ID:{openchannel.id} "
+            f'<a href="https://t.me/{openchannel.username}" style="color:#0000FF; text-decoration: none;">@{openchannel.username}</a>'
+            f"{admin_rights_html}"
         )
         openchannel_count += 1
         groups.append(openchannel)
@@ -426,11 +474,19 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
         count_row = closechannel_count if selection == '5' or selection == '0' else i
         owner = " (Владелец)" if closechannel.creator else ""
         admin = " (Администратор)" if closechannel.admin_rights is not None else ""
+        
+        # Получение списка прав администратора
+        admin_rights_list = get_admin_rights_channel_list(closechannel.admin_rights)
+        admin_rights_html = ""
+        if admin_rights_list:
+            admin_rights_html = "<ul style='font-size:14px; font-style:italic;'>" + "".join([f"<li style='margin-left:50px;'>{right}</li>" for right in admin_rights_list]) + "</ul>"
+        
         messages_count = f" / [{chat_message_counts.get(closechannel.id, 0)}]" if chat_message_counts else ""
         all_info.append(f"{count_row} - {closechannel.title} \033[93m[{closechannel.participants_count}]{messages_count}\033[0m \033[91m{owner} {admin}\033[0m ID:{closechannel.id}")
         private_channels_html.append(
             f'{closechannel_count}. <img src="{image_data_url}" alt=" " style="width:50px;height:50px;vertical-align:middle;margin-right:10px;">'
             f"<span style='color:#556B2F;'>{closechannel.title}</span> <span style='color:#8B4513;'>[{closechannel.participants_count}]</span> <span style='color:#FF0000;'>{owner} {admin}</span> ID:{closechannel.id}"
+            f"{admin_rights_html}"
         )
         closechannel_count += 1
         groups.append(closechannel)
@@ -460,6 +516,11 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
         count_row = opengroup_count if selection == '5' or selection == '0' else i
         owner = " (Владелец)" if openchat.creator else ""
         admin = " (Администратор)" if openchat.admin_rights is not None else ""
+        admin_rights_list = get_admin_rights_chat_list(openchat.admin_rights)
+        admin_rights_html = ""
+        if admin_rights_list:
+            admin_rights_html = "<ul style='font-size:14px; font-style:italic;'>" + "".join([f"<li style='margin-left:50px;'>{right}</li>" for right in admin_rights_list]) + "</ul>"
+        
         messages_count = f" / [{chat_message_counts.get(openchat.id, 0)}]" if chat_message_counts else ""
         all_info.append(f"{count_row} - {openchat.title} \033[93m[{openchat.participants_count}]{messages_count}\033[0m\033[91m {owner} {admin}\033[0m ID:{openchat.id} \033[94m@{openchat.username}\033[0m")
         public_groups_html.append(
@@ -467,6 +528,7 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
             f"<span style='color:#556B2F;'>{openchat.title}</span> <span style='color:#8B4513;'>[{openchat.participants_count}]</span> "
             f"<span style='color:#FF0000;'>{owner} {admin}</span> ID:{openchat.id} "
             f'<a href="https://t.me/{openchat.username}" style="color:#0000FF; text-decoration: none;">@{openchat.username}</a>'
+            f"{admin_rights_html}"
         )
         opengroup_count += 1
         groups.append(openchat)
@@ -496,12 +558,18 @@ def make_list_of_channels(delgroups, chat_message_counts, openchannels, closecha
         count_row = closegroup_count if selection == '5' or selection == '0' else i
         owner = " (Владелец)" if closechat.creator else ""
         admin = " (Администратор)" if closechat.admin_rights is not None else ""
+        admin_rights_list = get_admin_rights_chat_list(closechat.admin_rights)
+        admin_rights_html = ""
+        if admin_rights_list:
+            admin_rights_html = "<ul style='font-size:14px; font-style:italic;'>" + "".join([f"<li style='margin-left:50px;'>{right}</li>" for right in admin_rights_list]) + "</ul>"
+        
         messages_count = f" / [{chat_message_counts.get(closechat.id, 0)}]" if chat_message_counts else ""
         all_info.append(f"{count_row} - {closechat.title} \033[93m[{closechat.participants_count}]{messages_count}\033[0m \033[91m{owner} {admin}\033[0m ID:{closechat.id}")
         #private_groups_html.append(f"{closegroup_count} - <span style='color:#556B2F;'>{closechat.title}</span> <span style='color:#8B4513;'>[{closechat.participants_count}]</span> <span style='color:#FF0000;'>{owner} {admin}</span> ID:{closechat.id}")
         private_groups_html.append(
             f'{closegroup_count}. <img src="{image_data_url}" alt=" " style="width:50px;height:50px;vertical-align:middle;margin-right:10px;">'
             f"<span style='color:#556B2F;'>{closechat.title}</span> <span style='color:#8B4513;'>[{closechat.participants_count}]</span> <span style='color:#FF0000;'>{owner} {admin}</span> ID:{closechat.id}"
+            f"{admin_rights_html}"
         )
         closegroup_count += 1
         groups.append(closechat)
