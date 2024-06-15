607ddb869b6a86fb664be8042717ed7771587422
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-15 07:32:45 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -698,7 +698,7 @@ def get_messages_and_save_xcls(client, index: int, id_: bool, name: bool, group_
     if selection == '7':
         all_messages = client.iter_messages(group_title)
     else:
-        client.takeout() as takeout:
+       with client.takeout() as takeout:
             all_messages = takeout.iter_messages(group_title)
         
     #all_messages = client.iter_messages(group_title) if selection == '7' else takeout.iter_messages(group_title)  было
