0705759de28628b7a648a57e4a9b349247a75a43
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-16 11:20:47 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -78,15 +78,19 @@ def get_private_messages(client, target_user, selection):
                         # Загрузка фото в формате base64
                         photo_bytes = client.download_media(message.media.photo, file=BytesIO())
                         if photo_bytes:
-                            image = Image.open(BytesIO(photo_bytes))
+                            image = Image.open(photo_bytes)
+                            
+
                             original_size = image.size
                             new_size = (original_size[0] // 2, original_size[1] // 2)
                             image = image.resize(new_size)
+
+                            #image = image.resize((image.width // 2, image.height // 2))
                             output = BytesIO()
-                            image.save(output, format='JPEG', quality=50)
+                            image.save(output, format='JPEG', quality=70)
                             encoded_image = base64.b64encode(output.getvalue()).decode('utf-8')
                             image_data_url = f"data:image/jpeg;base64,{encoded_image}"
-                            media_type = f'<img src="{image_data_url}" alt="Photo" style="width: {new_size[0]}px; height: {new_size[1]}px;">'
+                            media_type = f'<img src="{image_data_url}" alt="Photo">'
 
                         
                             #encoded_image = base64.b64encode(photo_bytes.getvalue()).decode('utf-8')
