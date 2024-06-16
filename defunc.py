90808cddc90ff845002a409334344cc97a70c092
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-16 22:18:25 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -79,17 +79,17 @@ def get_private_messages(client, target_user, selection):
                 forward_sender = None
                 try:
                     forward_user = client.get_entity(message.forward.sender_id)
-                    forward_id = forward_user.id if hasattr(forward_user, 'id') else ''
-                    forward_first_name = forward_user.first_name if hasattr(forward_user, 'first_name') else ''
-                    forward_last_name = forward_user.last_name if hasattr(forward_user, 'last_name') else ''
-                    forward_username = forward_user.username if hasattr(forward_user, 'username') else ''
-                    #forward_sender = f"{forward_username} {forward_first_name} {forward_last_name} {forward_id}"
-                    forward_sender_parts = [part for part in [forward_username, forward_first_name, forward_last_name, forward_id] if (isinstance(part, str) and part.strip() != "") or isinstance(part, int)]
-                    forward_sender = " ".join(map(str, forward_sender_parts))
+                    forward_id = forward_user.id if hasattr(forward_user, 'id') else ' '
+                    forward_first_name = forward_user.first_name if hasattr(forward_user, 'first_name') else ' '
+                    forward_last_name = forward_user.last_name if hasattr(forward_user, 'last_name') else ' '
+                    forward_username = forward_user.username if hasattr(forward_user, 'username') else ' '
+                    forward_sender = f"{forward_username} {forward_first_name} {forward_last_name} {forward_id}"
+                    #forward_sender_parts = [part for part in [forward_username, forward_first_name, forward_last_name, forward_id] if (isinstance(part, str) and part.strip() != "") or isinstance(part, int)]
+                    #forward_sender = " ".join(map(str, forward_sender_parts))
 
                     
                 except Exception as e:
-                    forward_sender = 'не известный'
+                    forward_sender = 'неизвестный'
                 
         
             reply_text = None
