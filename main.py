a66ae4ba8099d39c704f52e67d5d1b865a340798
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-16 20:47:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -153,11 +153,11 @@ if __name__ == "__main__":
                               time.sleep(2)
                               all_info = []
                               os.system('cls||clear')
-                      #except ValueError:
-                           #print("Пожалуйста, выберите группу из списка")
-                           #time.sleep(2)
-                           #all_info = []
-                           #os.system('cls||clear')
+                      except ValueError:
+                           print("Пожалуйста, выберите группу из списка")
+                           time.sleep(2)
+                           all_info = []
+                           os.system('cls||clear')
            
        # 5 Выгрузить инфу об аккаунте
         elif selection == '5':
