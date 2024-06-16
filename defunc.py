4b4795dd81eba1b90d7f1648ff133a02658d73e6
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-16 21:00:19 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -79,11 +79,11 @@ def get_private_messages(client, target_user, selection):
                 forward_sender = None
                 try:
                     forward_user = client.get_entity(message.forward.sender_id)
-                    forward_id = forward_user.id if hasattr(forward_user, 'id') else 'Unknown ID'
+                    forward_id = f'id: {forward_user.id}` if hasattr(forward_user, 'id') else 'Unknown ID'
                     forward_first_name = forward_user.first_name if hasattr(forward_user, 'first_name') else ''
                     forward_last_name = forward_user.last_name if hasattr(forward_user, 'last_name') else ''
-                    forward_username = forward_user.username if hasattr(forward_user, 'username') else ''
-                    forward_sender = f"ID: {forward_id}, Name: {forward_first_name} {forward_last_name}, Username: {forward_username}"
+                    forward_username = f'@{forward_user.username}` if hasattr(forward_user, 'username') else ''
+                    forward_sender = f"Переслано от пользователя: id:{forward_username} {forward_first_name} {forward_last_name} {forward_id}"
                 except Exception as e:
                     forward_sender = 'первоисточник источник не известен'
                 
@@ -115,11 +115,8 @@ def get_private_messages(client, target_user, selection):
                             original_size = image.size
                             new_size = (original_size[0] // 2, original_size[1] // 2)
                             image = image.resize(new_size)
-
-                            #image = image.resize((image.width // 2, image.height // 2))
                             output = BytesIO()
-                            #quality = input('Введите процент сохранения качества, рекомендуется около 90)
-                            image.save(output, format='JPEG', quality=90)
+                            image.save(output, format='JPEG', quality=70)
                             encoded_image = base64.b64encode(output.getvalue()).decode('utf-8')
                             image_data_url = f"data:image/jpeg;base64,{encoded_image}"
                             media_type = f'<img src="{image_data_url}" alt="Photo">'
