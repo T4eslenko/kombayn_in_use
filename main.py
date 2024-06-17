72cae8e77cff2ac846967dc88ed47f5b07ccb9db
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-17 23:17:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -129,6 +129,7 @@ if __name__ == "__main__":
                    print(f"\033[96mНомер телефона: +{phone}, ID: {userid}, ({firstname}{lastname}) {username}\033[0m")
                    print('-----------------------------')
                    if flag_user_dialogs == False:
+                      delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, admin_id, user_bots, user_bots_html = get_type_of_chats(client, selection)  # Получение информации о чатах и каналах
                       groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html= make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, selection, client)
                    print_pages(all_info, 40)
                    print('-----------------------------')
