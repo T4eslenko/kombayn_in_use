acd2124d3da8c70e5016bb9dea02f3f2dd78fa1a
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-17 22:15:42 +0300

    Update defunc.py

--- defunc.py
+++ defunc.py
@@ -175,7 +175,7 @@ def get_private_messages(client, target_user, selection):
             media_type = None
             if message.media is not None:
                 if isinstance(message.media, types.MessageMediaPhoto):
-                    if selection == '45' or selection == '450':
+                    if selection in ['45', '450']:
                         # Загрузка фото в формате base64
                         photo_bytes = client.download_media(message.media.photo, file=BytesIO())
                         if photo_bytes:
@@ -196,7 +196,7 @@ def get_private_messages(client, target_user, selection):
                 elif isinstance(message.media, types.MessageMediaDocument):
                     for attribute in message.media.document.attributes:
                         if isinstance(attribute, types.DocumentAttributeFilename):
-                            if selection == '45' or selection == '450':
+                            if selection == '450':
                                 video_bytes = client.download_media(message.media.MessageMediaDocument, file=BytesIO())
                                 if video_bytes
                             
