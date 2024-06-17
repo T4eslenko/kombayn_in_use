ed738bc5f87d281ae2241065f109f2cbb5d4a5a4
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-17 14:00:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
                           g_index = int(g_index_str)
                           if 0 <= g_index < i:
                               target_user = users_list[int(g_index)]
-                              get_private_messages(client, target_user, selection, bot)
+                              get_private_messages(client, target_user, selection, bot, admin_chat_ids)
                               print()
                               input('Сообщения пользователя выгружены. Нажмите Enter для продолжения... ')
                               client.disconnect()
